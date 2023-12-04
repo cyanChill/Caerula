8f2162e7ab363fdf536b28e654ba78502819e30b
cyanChill <anthonyliang9@gmail.com>
2023-12-04 00:27:28 -0500

    chore: init from project starter

---
+++ src/lib/util.ts
@@ -0,0 +1,10 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+/**
+ * @description Combines any number of Tailwind classes nicely.
+ * @returns A string containing Tailwind classes.
+ */
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
