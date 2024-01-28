8095ba14b7e168a978fb21984903e31815b67a62
cyanChill <anthonyliang9@gmail.com>
2024-01-27 21:33:40 -0500

    chore: Display operator skills & operator page grid layout

--- src/app/operator/[...slug]/page.tsx
+++ src/app/operator/[...slug]/page.tsx
@@ -3,12 +3,28 @@ import { notFound } from "next/navigation";
 
 import OperatorTable from "@/data/operator/operatorTable.json";
 import ProfileTable from "@/data/operator/profile/profileTable.json";
+import SkillTable from "@/data/gameplay/skillTable.json";
 import SkinTable from "@/data/operator/skinTable.json";
+import TokenTable from "@/data/token/tokenTable.json";
 import { OpSlugTable } from "@/data/operator/slugTable";
 import VoiceTable from "@/data/operator/profile/voiceTable.json";
 
+import { cn } from "@/lib/style";
 import { constructMetadata } from "@/lib/metadata";
 import Overview from "./_components/overview";
+
+import {
+  type Recipient,
+  ExperienceProvider,
+} from "@/features/characters/Experience.store";
+import Experience from "@/features/characters/Experience";
+import Talent from "@/features/characters/Talent";
+import Skills from "@/features/characters/Skills";
+import Potentials, {
+  PotentialProvider,
+} from "@/features/characters/Potentials";
+import Trait from "@/features/characters/Trait";
+import Network from "@/features/characters/Network";
 
 interface Props {
   params: { slug: string[] };
@@ -60,6 +76,32 @@ export default function Operator({ params }: Props) {
     VoiceTable.opVoiceMap[opId].map((cvId) => [cvId, VoiceTable.cvTable[cvId]]),
   );
 
+  const statRecipients: Recipient[] = [
+    {
+      id: operator.id,
+      href: `/operator/${operator.slug}`,
+      name: operator.displayName,
+      range: operator.range,
+      stats: operator.stats,
+      bonus: operator.trustBonus,
+      iconId: operator.id,
+    },
+    ...(operator.tokensUsed ?? []).map((tokenId) => {
+      const { id, slug, displayName, range, stats, iconId } =
+        TokenTable[tokenId];
+      return {
+        ...{ id, range, stats, iconId },
+        href: `/token/${slug}`,
+        name: displayName,
+      };
+    }),
+  ];
+
+  const skills = operator.skills.map(({ id, tokenUsed }) => ({
+    ...SkillTable[id],
+    ...(tokenUsed ? { tokenName: TokenTable[tokenUsed].displayName } : {}),
+  }));
+
   return (
     <main className="mx-auto mb-[5svh] max-w-screen-2xl p-2 @container">
       <Overview
@@ -73,6 +115,26 @@ export default function Operator({ params }: Props) {
         skins={skins}
         cvTable={voices}
       />
+      <ExperienceProvider recipients={statRecipients}>
+        <PotentialProvider numPotentials={operator.potentials.length}>
+          <div
+            className={cn(
+              "grid grid-flow-dense grid-cols-2 gap-2 py-8 min-[350px]:gap-4 sm:px-4",
+              "md:auto-rows-fr md:grid-cols-4 lg:grid-cols-5",
+            )}
+          >
+            <Experience />
+            <Talent talents={operator.talents} />
+            <Skills skills={skills} />
+            <Potentials potentials={operator.potentials} />
+            <Trait
+              profession={operator.profession}
+              branchId={operator.branch}
+            />
+            <Network network={operator.affiliation} />
+          </div>
+        </PotentialProvider>
+      </ExperienceProvider>
     </main>
   );
 }
